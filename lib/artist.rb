2575556c7572bb01b8533c49ccb68df8c4a32d43
Becky Rosa <becky_m_rosa@yahoo.com>
2021-04-27 03:19:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,15 +25,13 @@ class Artist
 end
 
   def self.find_by_name(name)
-
-  end
-
-  def self.create_by_name(name)
-
+    self.all.find do |artist|
+      artist.name == name
+    end
   end
 
   def self.find_or_create_by_name(name)
-
+    self.find_by_name(name) || Artist.new
   end
 
   def print_songs
