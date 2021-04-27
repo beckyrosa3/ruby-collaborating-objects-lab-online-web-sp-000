a03ff96ce80667559595de27faab60f3d3e4cfa6
Becky Rosa <becky_m_rosa@yahoo.com>
2021-04-27 02:22:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,9 +7,26 @@ class Artist
   def initialize(name)
     @name = name
     @@all << self
+    @songs = []
   end
 
   def self.all
     @@all
+  end
+
+  def add_song
+
+  end
+
+  def songs
+
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def print_songs
+
   end
 end
