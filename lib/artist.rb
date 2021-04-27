f8851a0b90eca82a894790e4808f711240b1ebbc
Becky Rosa <becky_m_rosa@yahoo.com>
2021-04-27 03:25:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,12 @@
 class Artist
 
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
-    @songs = []
   end
 
   def self.all
@@ -35,6 +34,6 @@ end
   end
 
   def print_songs
-
+    
   end
 end
